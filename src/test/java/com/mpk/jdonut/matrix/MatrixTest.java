365784923ea66040177463122918d96f71853aee
Matthew Kelly <mattkelly048@gmail.com>
2025-08-07 19:24:54 +0200

    Update tests with changes made to Matrix class

--- src/test/java/com/mpk/jdonut/matrix/MatrixTest.java
+++ src/test/java/com/mpk/jdonut/matrix/MatrixTest.java
@@ -8,135 +8,135 @@ class MatrixTest {
 
     @Test
     void testAdd() {
-        var matrix1 = new Matrix(new double[][]{
+        var matrix1 = Matrix.of(new double[][]{
                 {1, 1, 1},
                 {1, 1, 1},
                 {1, 1, 1}
         });
 
-        var matrix2 = new Matrix(new double[][]{
+        var matrix2 = Matrix.of(new double[][]{
                 {1, 1, 1},
                 {1, 1, 1},
                 {1, 1, 1}
         });
 
-        var expected = new Matrix(new double[][]{
+        var expected = Matrix.of(new double[][]{
                 {2, 2, 2},
                 {2, 2, 2},
                 {2, 2, 2}
         });
 
-        var actual = Matrix.add(matrix1, matrix2);
+        var actual = matrix1.add(matrix2);
 
         assertEquals(expected, actual);
     }
 
     @Test
     void testSubtract() {
-        var matrix1 = new Matrix(new double[][]{
+        var matrix1 = Matrix.of(new double[][]{
                 {1, 1, 1},
                 {1, 1, 1},
                 {1, 1, 1}
         });
 
-        var matrix2 = new Matrix(new double[][]{
+        var matrix2 = Matrix.of(new double[][]{
                 {1, 1, 1},
                 {1, 1, 1},
                 {1, 1, 1}
         });
 
-        var expected = new Matrix(new double[][]{
+        var expected = Matrix.of(new double[][]{
                 {0, 0, 0},
                 {0, 0, 0},
                 {0, 0, 0}
         });
 
-        var actual = Matrix.subtract(matrix1, matrix2);
+        var actual = matrix1.subtract(matrix2);
 
         assertEquals(expected, actual);
     }
 
     @Test
     void testMultiply() {
-        var matrix1 = new Matrix(new double[][]{
+        var matrix1 = Matrix.of(new double[][]{
                 {2, 2, 2},
                 {2, 2, 2},
                 {2, 2, 2}
         });
 
-        var matrix2 = new Matrix(new double[][]{
+        var matrix2 = Matrix.of(new double[][]{
                 {3, 3, 3},
                 {3, 3, 3},
                 {3, 3, 3}
         });
 
-        var expected = new Matrix(new double[][]{
+        var expected = Matrix.of(new double[][]{
                 {6, 6, 6},
                 {6, 6, 6},
                 {6, 6, 6}
         });
 
-        var actual = Matrix.multiply(matrix1, matrix2);
+        var actual = matrix1.multiply(matrix2);
 
         assertEquals(expected, actual);
     }
 
     @Test
     void testDivide() {
-        var matrix1 = new Matrix(new double[][]{
+        var matrix1 = Matrix.of(new double[][]{
                 {6, 6, 6},
                 {6, 6, 6},
                 {6, 6, 6}
         });
 
-        var matrix2 = new Matrix(new double[][]{
+        var matrix2 = Matrix.of(new double[][]{
                 {2, 2, 2},
                 {2, 2, 2},
                 {2, 2, 2}
         });
 
-        var expected = new Matrix(new double[][]{
+        var expected = Matrix.of(new double[][]{
                 {3, 3, 3},
                 {3, 3, 3},
                 {3, 3, 3}
         });
 
-        var actual = Matrix.divide(matrix1, matrix2);
+        var actual = matrix1.divide(matrix2);
 
         assertEquals(expected, actual);
     }
 
     @Test
     void testMatmul() {
-        var matrix1 = new Matrix(new double[][]{
+        var matrix1 = Matrix.of(new double[][]{
                 {1, 2, 3},
                 {4, 5, 6}
         });
 
-        var matrix2 = new Matrix(new double[][]{
+        var matrix2 = Matrix.of(new double[][]{
                 {7, 8},
                 {9, 10},
                 {11, 12}
         });
 
-        var expected = new Matrix(new double[][]{
+        var expected = Matrix.of(new double[][]{
                 {58, 64},
                 {139, 154},
         });
 
-        var actual = Matrix.matmul(matrix1, matrix2);
+        var actual = matrix1.matMul(matrix2);
 
         assertEquals(expected, actual);
     }
 
     @Test
     void testTranspose() {
-        var matrix = new Matrix(new double[][]{
+        var matrix = Matrix.of(new double[][]{
                 {1, 2, 3},
                 {4, 5, 6}
         });
 
-        var expected = new Matrix(new double[][]{
+        var expected = Matrix.of(new double[][]{
                 {1, 4},
                 {2, 5},
                 {3, 6}
